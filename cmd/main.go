73d352e551236d24f897be4f3d5135a485280480
Wilfred Dube <wilfreddube@gmail.com>
2022-06-24 23:26:52 +0200

    Make snippet creation idempotent

--- cmd/main.go
+++ cmd/main.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
 )
 
 // Home diplays the home page containing a list of all snippets.
@@ -16,9 +17,26 @@ func ShowSnippet(c *gin.Context) {
 	c.String(http.StatusOK, "Display a specific snippet...")
 }
 
-// CreateSnippet creates a snippet. The creation is idempotent.
+// PrepareSnippet prepares an URL with a UUID for the creation of a snippets. Makes the
+// creation idempotent.
+func PrepareSnippet(c *gin.Context) {
+	id := c.Param("id")
+
+	if len(id) == 0 {
+		// Generate GUID to make call idempotent
+		location := c.FullPath() + "/" + uuid.NewString()
+
+		c.Redirect(http.StatusMovedPermanently, location)
+	}
+
+	c.String(http.StatusOK, "Create new snippet..."+id)
+}
+
+// CreateSnippet creates a snippet. Call is idempotent.
 func CreateSnippet(c *gin.Context) {
-	c.String(http.StatusOK, "Create new snippet...")
+	id := c.Param("id")
+
+	c.String(http.StatusOK, "Create new snippet..."+id)
 }
 
 // Creates a router
