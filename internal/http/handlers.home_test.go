269cbe1bb9e528067d637b8e40f5ae63608abe7b
Wilfred Dube <wilfreddube@gmail.com>
2022-06-25 15:14:58 +0200

    Home controller test

---
+++ internal/http/handlers.home_test.go
@@ -0,0 +1,23 @@
+package http
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestHomeController(t *testing.T) {
+	router := gin.Default()
+	router.GET("/", Home)
+	router.LoadHTMLGlob("../../ui/html/*")
+
+	w := httptest.NewRecorder()
+
+	req, _ := http.NewRequest("GET", "/", nil)
+	router.ServeHTTP(w, req)
+
+	assert.Equal(t, http.StatusOK, w.Code)
+}
