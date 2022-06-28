753caee95c42ac779230ac27f2fbabd23fab787b
Wilfred Dube <wilfreddube@gmail.com>
2022-06-28 21:04:22 +0200

    Atttach methods to data objects

--- internal/app.go
+++ internal/app.go
@@ -14,14 +14,18 @@ type Application struct {
 }
 
 // Creates a router
-func Routes() *gin.Engine {
+func (app *Application) Routes() *gin.Engine {
 	r := gin.Default()
 
-	r.Handle("GET", "/", http.Home)
-	r.Handle("GET", "/snippet", http.ShowSnippet)
-	r.Handle("GET", "/snippet/create", http.PrepareSnippet)
-	r.Handle("GET", "/snippet/create/:id", http.PrepareSnippet)
-	r.Handle("POST", "/snippet/create/:id", http.CreateSnippet)
+	// TODO: Use app.handler.Pusher() -> HTTP/2 & https required
+	r.Static("/assets", "ui/assets")
+	r.LoadHTMLGlob("ui/html/*")
+
+	r.Handle("GET", "/", app.handler.Home)
+	r.Handle("GET", "/snippet", app.handler.ShowSnippet)
+	r.Handle("GET", "/snippet/create", app.handler.PrepareSnippet)
+	r.Handle("GET", "/snippet/create/:id", app.handler.PrepareSnippet)
+	r.Handle("POST", "/snippet/create/:id", app.handler.CreateSnippet)
 
 	return r
 }
