f86dd03062bc698e5b8a6794dfd052d89ba6fcb3
Wilfred Dube <wilfreddube@gmail.com>
2022-07-02 18:21:25 +0200

    Fix test

--- internal/http/handlers.home_test.go
+++ internal/http/handlers.home_test.go
@@ -1,11 +1,18 @@
 package http
 
 import (
+	"database/sql"
+	"fmt"
+	"html/template"
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
+	"os"
 	"strings"
 	"testing"
+
+	"github.com/WilfredDube/ginny/internal/db"
+	_ "github.com/jackc/pgx/v4/stdlib"
 
 	"github.com/gin-gonic/gin"
 	"github.com/stretchr/testify/assert"
@@ -15,17 +22,44 @@ func init() {
 	gin.SetMode(gin.ReleaseMode)
 }
 
+func openDB(dsn string) (*db.Queries, error) {
+	conn, err := sql.Open("pgx", dsn)
+	if err != nil {
+		return nil, fmt.Errorf("pgx.Connect%w", err)
+	}
+
+	if err = conn.Ping(); err != nil {
+		return nil, err
+	}
+
+	db := db.New(conn)
+
+	return db, nil
+}
 func TestHomeController(t *testing.T) {
-	handler := &SnippetHandler{}
+	db, err := openDB(os.Getenv("$SNIPPET_DSN"))
+	if err != nil {
+		return
+	}
+
+	handler := &SnippetHandler{
+		DB: db,
+	}
 
 	router := gin.Default()
 
 	router.GET("/", handler.Home)
+
+	router.SetFuncMap(template.FuncMap{
+		"TrimGuid":       TrimGuid,
+		"HumanDate":      HumanDate,
+		"HumanDateShort": HumanDateShort,
+	})
+
 	router.LoadHTMLGlob("../../ui/html/*")
 
 	w := httptest.NewRecorder()
-
-	req, _ := http.NewRequest("GET", "/", nil)
+	req, _ := http.NewRequest(http.MethodGet, "/", nil)
 	router.ServeHTTP(w, req)
 
 	p, err := ioutil.ReadAll(w.Body)
